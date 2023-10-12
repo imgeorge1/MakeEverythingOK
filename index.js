b42f79e63cd7224f5de4b51fad284724d22fdad8
imgeorge1 <127306696+imgeorge1@users.noreply.github.com>
2023-10-12 13:11:45 +0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,43 @@
+
+document.getElementsByClassName("progress")[0].classList.add("hide");
+document.getElementsByClassName("done")[0].classList.add("hide");
+
+function Close() {
+    document.getElementsByClassName("progress")[0].classList.add("hide");
+    document.getElementsByClassName("done")[0].classList.add("hide");
+    document.getElementsByClassName("pushable")[0].classList.remove("blur");
+    document.getElementsByClassName("progress")[0].classList.remove("blur");
+}
+
+function Meo(){
+    var click = new Audio("btnClick.mp3");
+    click.play()
+
+    document.getElementsByClassName("pushable")[0].classList.add("blur");
+    document.getElementsByClassName("progress")[0].classList.remove("hide");
+
+    move();
+}
+
+var i = 0;
+function move() {
+  if (i == 0) {
+    i = 1;
+    var elem = document.getElementById("myBar");
+    var width = 10;
+    var id = setInterval(frame, 30);
+    function frame() {
+      if (width >= 100) {
+        clearInterval(id);
+        i = 0;
+        document.getElementsByClassName("done")[0].classList.remove("hide");
+        document.getElementsByClassName("progress")[0].classList.add("blur");
+      } else {
+        width++;
+        elem.style.width = width + "%";
+        elem.innerHTML = width  + "%";
+      }
+    }
+  }
+}
+
